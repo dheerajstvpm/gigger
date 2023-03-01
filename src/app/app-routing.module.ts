f523ab732c83ae486a12e25a6660e2dd8d746d42
Dheeraj S <dheerajstvpm@gmail.com>
2023-03-02 01:47:28 +0530

    path change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ import { OtherProfileComponent } from './components/other-profile/other-profile.
 
 const routes: Routes = [
   {
-    path: "",
+    path: "app",
     redirectTo: "/user/home",
     pathMatch: "full"
   },
