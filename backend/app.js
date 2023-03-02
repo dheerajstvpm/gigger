999d70fc54b5862c2187e90f2ded026ccf01cbd6
Dheeraj S <dheerajstvpm@gmail.com>
2023-03-02 20:45:29 +0530

    port 3000 app route change

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'))
 app.use(express.json());
 app.use('/api', api)
-app.get('/', function (req, res) {
+app.get('/app', function (req, res) {
     res.send('From app.js')
 })
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ import { OtherProfileComponent } from './components/other-profile/other-profile.
 
 const routes: Routes = [
   {
-    path: "app",
+    path: "",
     redirectTo: "/user/home",
     pathMatch: "full"
   },
